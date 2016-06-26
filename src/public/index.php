f8224d08cc9f9a4650651f56e7cce7070cbc137e
Shivam Khandelwal <skbly7@gmail.com>
2016-06-27 01:26:29 +0530

    Updated the first iteration of source code for Amber Timegate

--- src/public/index.php
+++ src/public/index.php
@@ -4,11 +4,67 @@ use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../vendor/autoload.php';
 
-$app = new \Slim\App;
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
+// Initialize Slim Application
+$settings = (include __DIR__ . '/../settings.php');
+$app = new \Slim\App($settings);
 
-    return $response;
+// Adding containers to Slim Application
+$container = $app->getContainer();
+$container['db'] = function ($container) {
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule->addConnection($container['settings']['db']);
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+    return $capsule;
+};
+
+// Functions
+function get_urir_id($url, $db, $insert=TRUE) {
+    $filter = ['url' => $url];
+    $id = $db->table('amber_urir')->where($filter)->get();
+    if($id == NULL && $insert) {
+        $id = $db->table('amber_urir')->insertGetId($filter);
+    }
+    else {
+        $id = $id[0]->id;
+    }
+    return $id;
+}
+
+// Routes
+$app->get('/list/{id:[0-9]+}', function (Request $request, Response $response) {
+    $id = $request->getAttribute('id');
+    $result = $this->db->table('amber_node')->find($id);
+    return $response->withJson($result);
 });
+
+$app->post('/add', function (Request $request, Response $response) {
+    $url = $request->getParam('url', '');
+    $urir_id = get_urir_id($url, $this->db);
+    $node_id = $request->getParam('node_id', NULL);
+    $cache_id = $request->getParam('cache_id', '');
+    $timestamp = $request->getParam('timestamp', time());
+    $result = $this->db->table('amber_urim')->updateOrInsert([
+        'urir_id' => $urir_id,
+        'node_id' => $node_id,
+    ],[
+        'cache_id' => $cache_id,
+        'timestamp' => $timestamp
+    ]);
+    return $response->withJson($result);
+});
+
+$app->post('/remove', function (Request $request, Response $response) {
+    $node_id = $request->getParam('node_id', NULL);
+    $cache_id = $request->getParam('cache_id', '');
+    $row = $this->db->table('amber_urim')->where('cache_id', '=', $cache_id)->where('node_id', '=', $node_id)->get();
+    $deleted = false;
+    foreach($row as $record) {
+        $deleted = true;
+        $result = $this->db->table('amber_urim')->delete($record->id);
+    }
+    return $response->withJson($deleted);
+});
+
+// Go, Go, Go!
 $app->run();
