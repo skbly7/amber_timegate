50bd25e73362dd2c5e861dc01af0978dc0254bd9
Shivam Khandelwal <skbly7@gmail.com>
2016-08-23 22:41:23 +0530

    Added latest database schema

--- dump.sql
+++ dump.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.47, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.49, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: amber_timegate
 -- ------------------------------------------------------
--- Server version	5.5.47-0ubuntu0.14.04.1-log
+-- Server version	5.5.49-0ubuntu0.14.04.1-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,29 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `amber_delete_request`
+--
+
+DROP TABLE IF EXISTS `amber_delete_request`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `amber_delete_request` (
+  `node_id` int(11) DEFAULT NULL,
+  `hash` varchar(32) DEFAULT NULL,
+  `timestamp` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `amber_delete_request`
+--
+
+LOCK TABLES `amber_delete_request` WRITE;
+/*!40000 ALTER TABLE `amber_delete_request` DISABLE KEYS */;
+/*!40000 ALTER TABLE `amber_delete_request` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `amber_node`
 --
 
@@ -26,11 +49,11 @@ CREATE TABLE `amber_node` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(256) NOT NULL DEFAULT 'NULL',
   `email` varchar(100) NOT NULL DEFAULT 'NULL',
-  `phone` int(11) DEFAULT NULL,
-  `public_key` mediumtext,
+  `key` varchar(64) NOT NULL,
   `is_verified` int(11) DEFAULT NULL,
+  `alive` int(11) DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COMMENT='This table contain information of all the Nodes.';
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1 COMMENT='This table contain information of all the Nodes.';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,7 +62,6 @@ CREATE TABLE `amber_node` (
 
 LOCK TABLES `amber_node` WRITE;
 /*!40000 ALTER TABLE `amber_node` DISABLE KEYS */;
-INSERT INTO `amber_node` VALUES (1,'http://amber.okrdx.com/','skbly7@gmail.com',2147483647,'public-key',1),(2,'http://drupal.okrdx.com/','skbly7@gmail.com',2147483647,'public-key',1);
 /*!40000 ALTER TABLE `amber_node` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -81,7 +103,7 @@ CREATE TABLE `amber_urim` (
   `cache_id` varchar(50) DEFAULT NULL,
   `urir_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=394 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -104,7 +126,7 @@ CREATE TABLE `amber_urir` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `url` varchar(1024) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=101 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -125,4 +147,3 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-27  1:28:59
